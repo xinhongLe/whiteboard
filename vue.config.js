c43bb598a75980d201962d01f6ff99535bdac458
moneyinto <woyaoqianjin@gmail.com>
2022-06-30 14:15:20 +0800

    打包圆规图片引用问题处理

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,26 @@ module.exports = defineConfig({
     transpileDependencies: true,
     productionSourceMap: false,
     css: { extract: false },
+    chainWebpack: config => {
+        config.module
+            .rule("images")
+            .test(/\.(png|jpe?g|gif|webp|svg)(\?.*)?$/)
+            .set("parser", {
+                dataUrlCondition: {
+                    maxSize: 1000 * 1024 // 10KiB
+                }
+            })
+        if (process.env.NODE_ENV === "production") {
+            // 移除 prefetch 插件
+            config.plugins.delete("prefetch");
+            // 移除 preload 插件
+            config.plugins.delete("preload");
+        }
+    },
     configureWebpack: {
         externals:
             process.env.NODE_ENV === "development"
                 ? []
                 : ["vue", "core-js", "lodash", "perfect-freehand"]
-    }
+    },
 });
